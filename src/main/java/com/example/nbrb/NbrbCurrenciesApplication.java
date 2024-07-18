2116df6a54e8a988179799d725a658a4c9bdff32
Aliaksandr <dyvedd@gmail.com>
2024-07-18 21:59:25 +0300

    Initial commit

---
+++ src/main/java/com/example/nbrb/NbrbCurrenciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbrbCurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbrbCurrenciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nbrb/NbrbCurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbrbCurrenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
